17ecca01e2e38d1ad4c3b977960c49d049c680be
Guillermo Grillo <guillermo.grillo88@gmail.com>
2017-03-26 14:51:30 -0300

    Commit del esqueleto del proyecto

---
+++ showMustGoOn/src/main/java/com/tacs/ShowMustGoOnApplication.java
@@ -0,0 +1,12 @@
+package com.tacs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowMustGoOnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShowMustGoOnApplication.class, args);
+	}
+}
---
+++ showMustGoOn/src/test/java/com/tacs/ShowMustGoOnApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tacs;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ShowMustGoOnApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
