58cbebd30963756d1c287c311dc00b2ae5335cfe
Alejandro Hierro <alejandrohierro@gmail.com>
2017-04-01 18:25:58 -0300

    Ejemplo de junit para controllers

--- src/test/java/ar/com/tacs/grupo5/frba/utn/AppTest.java
+++ src/test/java/ar/com/tacs/grupo5/frba/utn/AppTest.java
@@ -1,38 +1,48 @@
 package ar.com.tacs.grupo5.frba.utn;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.ClassRule;
+import org.junit.Test;
+
+import com.despegar.http.client.GetMethod;
+import com.despegar.http.client.HttpResponse;
+import com.despegar.sparkjava.test.SparkServer;
+
+import spark.servlet.SparkApplication;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
+public class AppTest
+
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+	public static class TestContollerTestSparkApplication implements SparkApplication {
+		@Override
+		public void init() {
+			new TestController();
+		}
+	}
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+	@ClassRule
+	public static SparkServer<TestContollerTestSparkApplication> testServer = new SparkServer<>(
+			AppTest.TestContollerTestSparkApplication.class, 8080);
+
+	@Test
+	public void testUsers() throws Exception {
+		GetMethod get = testServer.get("/api/users/", false);
+		HttpResponse httpResponse = testServer.execute(get);
+		assertEquals(200, httpResponse.code());
+		assertNotNull(httpResponse.body());
+	}
+}
+
+class TestController {
+
+	public TestController() {
+		App.main(null);
+	}
+
 }
