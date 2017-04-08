2d5f6448a55fb61958f50845f50ead6690743402
Alejandro Hierro <alejandrohierro@gmail.com>
2017-04-08 17:35:13 -0300

    Se configura spring spring

--- src/main/java/ar/com/tacs/grupo5/frba/utn/App.java
+++ src/main/java/ar/com/tacs/grupo5/frba/utn/App.java
@@ -8,70 +8,77 @@ import static spark.Spark.post;
 import static spark.Spark.put;
 import static spark.Spark.staticFiles;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
 import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 import ar.com.tacs.grupo5.frba.utn.controllers.ApiController;
 
+@Configuration
+@ComponentScan({ "ar.com.tacs.grupo5.frba.utn" })
 public class App {
 	private static final int PORT = 8080;
 
 	public static void main(String[] args) {
-		config();
 		port(PORT);
-		
+		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(App.class);
+		ApiController apiController = ctx.getBean(ApiController.class);
+		ctx.close();
 		staticFiles.location("/public");
 		path("/api", () -> {
 
 			path("/users", () -> {
-				get("/", ApiController.getUsers);
-				get("/:id/", ApiController.getUser);
-				get("/:id/favmovies/", ApiController.getUserFavMovies);
-				get("/:id/favactors/", ApiController.getserFavActors);
-				get("/:id/intersection/:id2/", ApiController.getUserIntersection);
-				get("/ranking/actor/", ApiController.getRankingActor);
+				get("/", apiController.getUser);
+				get("/:id/", apiController.getUser);
+				get("/:id/favmovies/", apiController.getUserFavMovies);
+				get("/:id/favactors/", apiController.getserFavActors);
+				get("/:id/intersection/:id2/", apiController.getUserIntersection);
+				get("/ranking/actor/", apiController.getRankingActor);
 			});
 			path("/user", () -> {
-				post("/register/", ApiController.registerUser);
-				post("/login/", ApiController.login);
-				post("/logout/", ApiController.logout);
+				post("/register/", apiController.registerUser);
+				post("/login/", apiController.login);
+				post("/logout/", apiController.logout);
 			});
 			path("/search", () -> {
-				get("/:type/", ApiController.searchBy);
+				get("/:type/", apiController.searchBy);
 			});
 			path("/favmovies", () -> {
-				post("/", ApiController.createNewList);
-				get("/:id/", ApiController.getFavMovieDetail);
-				put("/:id/", ApiController.updateFavMoviesDetail);
-				delete("/:id/", ApiController.deleteFavMoviesList);
-				post("/:id/movies/", ApiController.addMovieToList);
-				delete("/:id/movies/:movie_id/", ApiController.deleteMovieFromList);
-				get("/:id/intersection/:id2/", ApiController.getUserIntersection);
-				get("/:id/ranking/", ApiController.getRankingFromList);
+				post("/", apiController.createNewList);
+				get("/:id/", apiController.getFavMovieDetail);
+				put("/:id/", apiController.updateFavMoviesDetail);
+				delete("/:id/", apiController.deleteFavMoviesList);
+				post("/:id/movies/", apiController.addMovieToList);
+				delete("/:id/movies/:movie_id/", apiController.deleteMovieFromList);
+				get("/:id/intersection/:id2/", apiController.getUserIntersection);
+				get("/:id/ranking/", apiController.getRankingFromList);
 
 			});
 			path("/favactors", () -> {
-				post("/", ApiController.addActorToList);
-				delete("/:id/", ApiController.deleteActorFromList);
+				post("/", apiController.addActorToList);
+				delete("/:id/", apiController.deleteActorFromList);
 			});
 			path("/movie", () -> {
-				get("/:id/", ApiController.getMovieById);
+				get("/:id/", apiController.getMovieById);
 			});
 			path("/movies", () -> {
-				get("/recommended/", ApiController.getRecommendedMovies);
+				get("/recommended/", apiController.getRecommendedMovies);
 			});
 			path("/actor", () -> {
-				get("/:id/", ApiController.getActorById);
+				get("/:id/", apiController.getActorById);
 			});
 		});
 		System.out.println("La aplicación levantó correctamente y escucha en el puerto " + PORT);
 	}
 
-	private static void config() {
-		Gson gson = new GsonBuilder()
-			    .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
-			    .create();
-		ApiController.gson = gson;
+	@Bean
+	public Gson gson() {
+		return new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES).create();
 	}
+
 }
---
+++ src/main/java/ar/com/tacs/grupo5/frba/utn/service/ApiService.java
@@ -0,0 +1,8 @@
+package ar.com.tacs.grupo5.frba.utn.service;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ApiService {
+
+}
