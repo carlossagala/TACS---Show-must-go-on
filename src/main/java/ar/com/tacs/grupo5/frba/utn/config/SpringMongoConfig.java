67e91da4bebb9946f18251b73bf30ee1e5a626c8
Carlos <Carlos@DESKTOP-JNJ0UK7>
2017-05-28 19:38:53 -0300

    test: configuracion para coneccion de mongoDB con la app en openshift

--- src/main/java/ar/com/tacs/grupo5/frba/utn/config/SpringMongoConfig.java
+++ src/main/java/ar/com/tacs/grupo5/frba/utn/config/SpringMongoConfig.java
@@ -6,6 +6,8 @@ import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
 
 import com.mongodb.Mongo;
 import com.mongodb.MongoClient;
+import com.mongodb.MongoClientOptions;
+import com.mongodb.ServerAddress;
 
 @Configuration
 public class SpringMongoConfig extends AbstractMongoConfiguration {
@@ -18,7 +20,17 @@ public class SpringMongoConfig extends AbstractMongoConfiguration {
 	@Override
 	@Bean
 	public Mongo mongo() throws Exception {
-		return new MongoClient("127.0.0.1");
+		   String host = System.getenv("OPENSHIFT_MONGODB_DB_HOST");
+	        if (host == null) {
+	            return new MongoClient("127.0.0.1");
+	           
+	        }
+	        int port = Integer.parseInt(System.getenv("OPENSHIFT_MONGODB_DB_PORT"));
+	        String dbname = System.getenv("OPENSHIFT_APP_NAME");
+	        String username = System.getenv("OPENSHIFT_MONGODB_DB_USERNAME");
+	        String password = System.getenv("OPENSHIFT_MONGODB_DB_PASSWORD");
+	        MongoClientOptions mongoClientOptions = MongoClientOptions.builder().connectionsPerHost(20).build();
+	        return new MongoClient(new ServerAddress(host, port), mongoClientOptions);
 	}
 
 }
