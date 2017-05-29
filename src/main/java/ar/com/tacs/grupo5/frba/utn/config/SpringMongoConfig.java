7fa6cf219b8e8e8f0ab1f0bd87a187fc11f4c1e6
Unknown <alejandrohierro@gmail.com>
2017-05-29 20:19:15 -0300

    subo configuracion para pegarle al mongo openshift con port forwarding

--- src/main/java/ar/com/tacs/grupo5/frba/utn/config/SpringMongoConfig.java
+++ src/main/java/ar/com/tacs/grupo5/frba/utn/config/SpringMongoConfig.java
@@ -1,36 +1,42 @@
 package ar.com.tacs.grupo5.frba.utn.config;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
 
 import com.mongodb.Mongo;
 import com.mongodb.MongoClient;
-import com.mongodb.MongoClientOptions;
+import com.mongodb.MongoCredential;
 import com.mongodb.ServerAddress;
 
 @Configuration
 public class SpringMongoConfig extends AbstractMongoConfiguration {
-
-	@Override
-	public String getDatabaseName() {
-		return "show_must_go_on";
-	}
-
-	@Override
-	@Bean
-	public Mongo mongo() throws Exception {
-		   String host = System.getenv("OPENSHIFT_MONGODB_DB_HOST");
-	        if (host == null) {
-	            return new MongoClient("127.0.0.1");
-	           
-	        }
-	        int port = Integer.parseInt(System.getenv("OPENSHIFT_MONGODB_DB_PORT"));
-	        String dbname = System.getenv("OPENSHIFT_APP_NAME");
-	        String username = System.getenv("OPENSHIFT_MONGODB_DB_USERNAME");
-	        String password = System.getenv("OPENSHIFT_MONGODB_DB_PASSWORD");
-	        MongoClientOptions mongoClientOptions = MongoClientOptions.builder().connectionsPerHost(20).build();
-	        return new MongoClient(new ServerAddress(host, port), mongoClientOptions);
-	}
-
+	
+    @Value("${spring.data.mongodb.host}")
+    private String host;
+    @Value("${spring.data.mongodb.port}")
+    private Integer port;
+    @Value("${spring.data.mongodb.username}")
+    private String username;
+    @Value("${spring.data.mongodb.database}")
+    private String database;
+    @Value("${spring.data.mongodb.password}")
+    private String password;
+    
+    @Override
+    public String getDatabaseName() {
+        return database;
+    }
+    
+    @Override
+    @Bean
+    public Mongo mongo() throws Exception {
+        return new MongoClient(new ServerAddress(host, port),
+        		Arrays.asList(MongoCredential.createCredential(username,database, password.toCharArray())));
+    }
+    
+    
 }
