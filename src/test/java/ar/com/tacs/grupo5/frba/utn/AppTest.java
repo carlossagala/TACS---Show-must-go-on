63554a6839ba2429804272428c1cca79db14c0fb
Unknown <alejandrohierro@gmail.com>
2017-06-10 20:05:19 -0300

    Correccion IntegrationTests

--- src/test/java/ar/com/tacs/grupo5/frba/utn/AppTest.java
+++ src/test/java/ar/com/tacs/grupo5/frba/utn/AppTest.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 
@@ -14,28 +15,27 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 import ar.com.tacs.grupo5.frba.config.SpringMongoConfig;
+import ar.com.tacs.grupo5.frba.utn.dao.MongoTestConfiguration;
 
 @SpringBootApplication
 @PropertySource("classpath:application.properties")
-@ComponentScan({ "ar.com.tacs.grupo5.frba.utn" })
+@ComponentScan(basePackages = {"ar.com.tacs.grupo5.frba.utn"}, excludeFilters={
+		@ComponentScan.Filter(type=FilterType.ASSIGNABLE_TYPE, value=SpringMongoConfig.class)})
+public class AppTest {
+	private static Logger logger = LoggerFactory.getLogger(AppTest.class);
 
-public class App {
-	private static Logger logger = LoggerFactory.getLogger(App.class);
-	
-	public static void main(String[] args) {
-		@SuppressWarnings("resource")
-		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(App.class, SpringMongoConfig.class);
+	public AnnotationConfigApplicationContext mainTest(String[] args) {
+		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(AppTest.class, MongoTestConfiguration.class);
 		Environment environment = ctx.getBean(Environment.class);
 		String port = environment.getProperty("server.port");
 		logger.info("La aplicación levantó correctamente y escucha en el puerto " + port);
+		return  ctx;
 	}
-
+	
 	@Bean
 	public Gson gson() {
 		GsonBuilder gsonBuilder = new GsonBuilder();
 		gsonBuilder.setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES);
 		return gsonBuilder.create();
 	}
-
-
 }
